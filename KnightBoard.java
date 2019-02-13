bd256971d936e01d5789cd59a6b2fe27ca62f901
Samson Badlia <sbadlia00@stuy.edu>
2019-02-13 11:15:57 -0500

    Wrote toString (Tested), methods to clear and check if baord is clear, test cases for toString

--- KnightBoard.java
+++ KnightBoard.java
@@ -1,34 +1,77 @@
 public class KnightBoard{
 
+
+  public static void main(String[] args) {
+
+    KnightBoard k = new KnightBoard(2,2);
+    System.out.println(k);
+
+    KnightBoard l = new KnightBoard(2,5);
+    System.out.println(l);
+
+    KnightBoard m = new KnightBoard(5,2);
+    System.out.println(m);
+
+  }
+
+  int[][] board;
+
   /**
   @throws IllegalArgumentException when either parameter is negative.
   **/
   public KnightBoard(int startingRows,int startingCols){
-
+    board = new int[startingRows][startingCols];
   }
 
   public String toString(){
-
+    String s = "\n";
+    for (int i = 0; i < board.length; i++){
+      for (int j = 0; j < board[i].length;j++){
+        s += " 0 ";
+        if (j == board[i].length - 1) s += "\n";
+      }
+    }
+    return s;
   }
+
+  private void clear(){
+  for (int i = 0; i < board.length; i++){
+    for (int j = 0; j < board[0].length; j++){
+      board[i][j] = 0;
+      }
+    }
+  }
+
+  private boolean empty(){
+    for (int i = 0; i < board.length; i++){
+      for (int j = 0; j < board[0].length; j++){
+        if (board[i][j] != 0) return false;
+        }
+      }
+      return true;
+    }
+
 
  /**
   @throws IllegalStateException when the board contains non-zero values.
   @throws IllegalArgumentException when either parameter is negative or out of bounds.
    **/
   public boolean solve(int startingRow, int startingCol){
-
-  }
+    if (!empty()) throw new IllegalStateException();
+    if (startingRow < 0 || startingCol < 0 || startingRow > board.length || startingCol > board[0].length) throw new IllegalArgumentException();
+    return true;
+   }
 
   /**
   @throws IllegalStateException when the board contains non-zero values.
   @throws IllegalArgumentException when either parameter is negative or out of bounds.
    **/
   public int countSolutions(int startingRow, int startingCol){
-
+    return 0;
   }
 
   private boolean solveH(int row ,int col, int level){
-
+    return true;
   }
 
 
