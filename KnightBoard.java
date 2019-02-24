30186195a362f3e25c9412f56e4309a10c6737b8
Samson Badlia <sbadlia00@stuy.edu>
2019-02-24 00:10:42 -0500

    Attempted to fix helper for count unsuccessful

--- KnightBoard.java
+++ KnightBoard.java
@@ -4,16 +4,20 @@ public class KnightBoard{
 
 
   public static void main(String[] args) {
-
-    KnightBoard k = new KnightBoard(5,5);
-    System.out.println(k.solve(0,0));
-    System.out.println("Should be true");
-    System.out.println(k.countSolutions(0,0));
-    System.out.println("Should be 304");
-
-  }
+      KnightBoard kb = new KnightBoard(5, 5);
+      long timeStart = System.currentTimeMillis();
+      int solutionsFound = 0;
+      for(int i = 0; i < 5; i++){
+          for(int j = 0; j < 5; j++){
+              solutionsFound += kb.countSolutions(i, j);
+          }
+      }
+    }
 
   int[][] board;
+  int[][] moves = new int[][]{
+    {2,1} , {1,2} , {-1,2} , {-2,1}, {-2,-1}, {-1,-2}, {1,-2}, {2,-1}
+                              };
 
   /**
   @throws IllegalArgumentException when either parameter is negative.
@@ -25,8 +29,10 @@ public class KnightBoard{
   public boolean addKnight(int r, int c, int step){
       if (r < 0 || c < 0 || r >= board.length || c >= board[0].length) return false;
       if (board[r][c] != 0) return false;
-      board[r][c] = step;
-      return true;
+      else{
+        board[r][c] = step;
+        return true;
+      }
   }
 
   public boolean removeKnight(int r, int c){
@@ -57,12 +63,7 @@ public class KnightBoard{
   }
 
   private boolean empty(){
-    for (int i = 0; i < board.length; i++){
-      for (int j = 0; j < board[0].length; j++){
-        if (board[i][j] != 0) return false;
-        }
-      }
-      return true;
+      return board[0][0] != 0 ;
     }
 
  /**
@@ -70,23 +71,21 @@ public class KnightBoard{
   @throws IllegalArgumentException when either parameter is negative or out of bounds.
    **/
   public boolean solve(int startingRow, int startingCol){
-    if (!empty()) throw new IllegalStateException();
+    clear();
+    if (empty()) throw new IllegalStateException();
     if (startingRow < 0 || startingCol < 0 || startingRow > board.length || startingCol > board[0].length) throw new IllegalArgumentException();
-    return solveR(0,0,1);
+    return solveR(startingRow,startingCol,1);
    }
 
    private boolean solveR(int r, int c, int move){
      // tracks coordinate of next square
     int x,y;
     if (move == board.length * board[0].length) return true;
-    //all possible moves in x,y format
-    int[] moves = { 2,1, 1,2, -1,2, -2,1, -2,-1, -1,-2, 1,-2, 2,-1};
     //incrementing by 2 because the array is a pair of values (x,y)
-    for (int i = 0; i < moves.length; i += 2 ){
-      //location of next spot
-      x = r + moves[i];
-      y = c + moves[i+1];
+    for (int i = 0; i < moves.length - 1; i++ ){
       //checks if knight can be moved there
+      x = r + moves[i][0];
+      y = c + moves[i + 1][0];
       if (addKnight(x,y,move)){
         if (solveR(x,y,move + 1)) return true;
         else{
@@ -103,26 +102,22 @@ public class KnightBoard{
    **/
   public int countSolutions(int startingRow, int startingCol){
     clear();
-    if (!empty()) throw new IllegalStateException();
+    if (empty()) throw new IllegalStateException();
     if (startingRow < 0 || startingCol < 0 || startingRow > board.length || startingCol > board[0].length) throw new IllegalArgumentException();
-    return countR(0,0,1);
+    addKnight(startingRow,startingCol,1);
+    return countR(startingRow,startingCol,2);
   }
 
   private int countR(int r, int c, int move){
-    // tracks coordinate of next square
-   int x,y;
    int count = 0;
-   if (move == board.length * board[0].length) return 1;
-   //all possible moves in x,y format
-   int[] moves = { 2,1, 1,2, -1,2, -2,1, -2,-1, -1,-2, 1,-2, 2,-1};
+   if (move >= board.length * board[0].length) return 1;
    //incrementing by 2 because the array is a pair of values (x,y)
-   for (int i = 0; i < moves.length; i += 2 ){
-     //location of next spot
-     x = r + moves[i];
-     y = c + moves[i+1];
+   for (int i = 0; i < moves.length - 1  ; i++){
      //checks if knight can be moved there
-     if (addKnight(x,y,move)){
-       count += countR(x,y,move+1);
+     int x = r + moves[i][0];
+     int y = c + moves[i + 1][0];
+     if (addKnight(x,y, move)){
+       count += countR(x,y , move+1);
        removeKnight(x,y);
      }
    }
